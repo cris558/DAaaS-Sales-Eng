78979fba46e26fa8a7e89326d59f68151d2f2be9
cris558 <57805623+cris558@users.noreply.github.com>
2023-06-22 16:45:38 -0400

    Trying to create new external table, unsuccessful - from databricks; seems not possible in a serverless pool. Internet forums suggest using Unity Catalog.

--- notebooks/Users/cristian.hudici@statcan.gc.ca/Test.py
+++ notebooks/Users/cristian.hudici@statcan.gc.ca/Test.py
@@ -267,7 +267,7 @@ display(table_name)
 # COMMAND ----------
 
 import adal
-keyvault = "devsandbox"
+keyvault = "devsandbox-kv1"
  
 tenant = dbutils.secrets.get(scope = keyvault, key = "TenantID")
 login_url = "https://login.microsoftonline.com"
@@ -295,7 +295,7 @@ connectionProperties = {
     "hostNameInCertificate" : "*.database.windows.net" }
 
 pushdown_query = """(
-    SELECT * FROM testingdb.dbo.testing2
+    SELECT * FROM testingdb.dbo.Wildfires
   ) t"""
 
 df = spark.read.jdbc(url=jdbcUrl, table=pushdown_query, properties=connectionProperties)
@@ -303,8 +303,46 @@ display(df)
 
 # COMMAND ----------
 
-# MAGIC %sql
-# MAGIC -- USE testingdb;
-# MAGIC SELECT *
-# MAGIC FROM testingdb.dbo.testing2
-# MAGIC LIMIT 3
+import adal
+keyvault = "devsandbox-kv1"
+ 
+tenant = dbutils.secrets.get(scope = keyvault, key = "TenantID")
+login_url = "https://login.microsoftonline.com"
+authority_url = (login_url + '/' + tenant)
+ 
+context = adal.AuthenticationContext(authority_url, timeout=None)
+ 
+access_token = context.acquire_token_with_client_credentials(
+    "https://database.windows.net/",
+    dbutils.secrets.get(scope = keyvault, key = "DataBricksClientID"),
+    dbutils.secrets.get(scope = keyvault, key = "DataBricksSecret"))
+ 
+jdbcHostname = "dev-sandbox-syn-ondemand.sql.azuresynapse.net"
+jdbcDatabase = "testingdb"
+jdbcPort = 1433
+ 
+jdbcUrl = "jdbc:sqlserver://{0}:{1}".format(jdbcHostname, jdbcPort)
+ 
+connectionProperties = {   
+    "accessToken": access_token.get('accessToken'),  
+    "database": jdbcDatabase,  
+    "trustServerCertificate" : "false", 
+    "encrypt" : "true",  
+    "driver" : "com.microsoft.sqlserver.jdbc.SQLServerDriver",  
+    "hostNameInCertificate" : "*.database.windows.net" }
+
+pushdown_query = """(
+    SELECT * FROM testingdb.dbo.Wildfires
+  ) t"""
+
+# pushdown_query = """(
+#     SELECT * INTO testingdb.dbo.Wildfires_Backup
+#     FROM testingdb.dbo.Wildfires
+#     WHERE 1= 0
+#   ) t"""
+
+df = spark.read.jdbc(url=jdbcUrl, table=pushdown_query, properties=connectionProperties)
+display(df)
+
+# COMMAND ----------
+
